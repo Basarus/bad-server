9586a3b241c369bf99112ec8a075c56d288adc86
basarus51 <basarus51@gmail.com>
2024-12-09 21:52:38 +0300

    fix

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,13 +1,14 @@
 import { NextFunction, Request, Response, Router } from 'express'
 import NotFoundError from '../errors/not-found-error'
 
-import auth from '../middlewares/auth'
+import auth, { roleGuardMiddleware } from '../middlewares/auth'
 import authRouter from './auth'
 import customerRouter from './customers'
 import orderRouter from './order'
 import productRouter from './product'
 import uploadRouter from './upload'
 import { csrfProtection } from '../middlewares/crfProtection'
+import { Role } from '../models/user'
 
 const router = Router()
 
@@ -15,7 +16,7 @@ router.use('/auth', authRouter)
 router.use('/product', csrfProtection, productRouter)
 router.use('/order', auth, orderRouter)
 router.use('/upload', auth, uploadRouter)
-router.use('/customers', csrfProtection, auth, customerRouter)
+router.use('/customers', csrfProtection, auth, roleGuardMiddleware(Role.Admin), customerRouter)
 
 router.use((_req: Request, _res: Response, next: NextFunction) => {
     next(new NotFoundError('Маршрут не найден'))
